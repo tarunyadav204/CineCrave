52137d137979fdfa38fd07d93594f9720a46990b
tarun1508 <tarun204yadav@gmail.com>
2025-06-17 23:04:23 +0530

    backend-testApi

--- backend/server.js
+++ backend/server.js
@@ -17,6 +17,10 @@ await connectDB();
 
 app.use("/api/inngest", serve({ client: inngest, functions }));
 
+app.get("/", (req, res) => {
+    res.send("Server is running ....");
+})
+
 
 app.listen(PORT, () => {
     console.log(`Server is running on Port ${PORT}`);
