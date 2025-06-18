64eea0ef69a53df5486b8cff8a08ae4b491a9e36
tarun1508 <tarun204yadav@gmail.com>
2025-06-18 22:40:51 +0530

    fix db.js

--- backend/configs/db.js
+++ backend/configs/db.js
@@ -2,7 +2,8 @@ import mongoose from "mongoose";
 
 const connectDB = async () => {
     try {
-        await mongoose.connect(`${process.env.MONGODB_URL}/cineCrave`);
+        await mongoose.connect(`${process.env.MONGODB_URL}cineCrave`);
+        //console.log(`${process.env.MONGODB_URL}cineCrave`);
         console.log("✅ MongoDB connected successfully to cineCrave");
     } catch (error) {
         console.error("❌ MongoDB connection error:", error.message);
